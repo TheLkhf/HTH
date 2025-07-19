f003dbf26d0814f3ed4e87299337cff24c89642c
TheLkhf <purepestilence@gmail.com>
2025-07-19 13:13:54 -0700

    Create psuedocode.py

---
+++ psuedocode.py
@@ -0,0 +1,20 @@
+
+
+start
+
+1. Grab the calender click or open up the schedule app on phone.
+2. Set the appointment in schedule app.
+3. Set multiple noifications for schedule app.
+4. Set vibrate 15 minutes before appointment is due. 
+
+when app vibrates:
+1. dismiss app.
+    a. Give 5 more minutes.
+    b. Give 10 more minutes.
+
+    repeat till extra time is over
+    a. Give me 5 more minutes.
+    b. close the notification and head to appointment. 
+
+2. close notification and head to appointent.
+End
